c512e9cf179de899b8b9315296a0a52d5081d2b2
Camille <mille.ouriques@gmail.com>
2023-05-15 08:58:17 -0300

    app redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./shared/redux/slices";
+import { ToastContainer } from 'react-toastify';
+
+const store = configureStore({reducer: rootReducer});
+export type AppDispatch = typeof store.dispatch
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <ToastContainer style={{zIndex: 9999999}} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
